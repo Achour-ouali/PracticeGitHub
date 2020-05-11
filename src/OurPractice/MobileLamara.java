90565c4ceb5fdeed4b96161163b9ec4836a1ab25
LamaraBenz <lamara.eng@gmail.com>
2020-05-10 23:59:36 -0400

    updated work

--- src/OurPractice/MobileLamara.java
+++ src/OurPractice/MobileLamara.java
@@ -1,4 +1,18 @@
 package OurPractice;
 
 public class MobileLamara {
+    public String brand;
+    public int storageSize;
+
+    public MobileLamara() {
+    }
+
+    public MobileLamara(String brand) {
+        this.brand = brand;
+    }
+
+    public MobileLamara(String brand, int storageSize) {
+        this.brand = brand;
+        this.storageSize = storageSize;
+    }
 }
