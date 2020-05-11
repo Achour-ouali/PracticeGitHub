fd654a3f0112e067ef3cb3721fa79f569e0f922e
salah-mouloudi <mouloudisalah1989@gmail.com>
2020-05-10 23:46:48 -0400

    car first code

--- src/OurPractice/CarSalah.java
+++ src/OurPractice/CarSalah.java
@@ -1,4 +1,43 @@
 package OurPractice;
 
 public class CarSalah {
+    int a = 12;
+    String b = "ashour";
+    char d = '#';
+
+
+    public CarSalah() {
+    }
+
+    public CarSalah(int a) {
+        this.a = a;
+    }
+
+    public CarSalah(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public CarSalah(int a, String b, char d) {
+        this.a = a;
+        this.b = b;
+        this.d = d;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
